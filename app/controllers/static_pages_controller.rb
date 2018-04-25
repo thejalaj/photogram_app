650bc1d4616f57b896f65f1fcc4c08f13c88dba7
Jalaj <jalaj95@gmail.com>
2018-04-25 10:38:56 +0530

    fix_3_home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post  = current_user.posts.build
-      @posts=Post.where(user_id: current_user.following.pluck(:id) + [current_user.id]).order(updated_at: :desc).sort_by {|obj| obj.updated_at}.reverse
+      @posts=Post.where(user_id: current_user.following.pluck(:id) + [current_user.id]).order(updated_at: :desc)
     end
   end
 
